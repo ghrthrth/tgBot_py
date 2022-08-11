4a008277185834cb96bf37fb6942128f2e1053cb
ghrthrth <dima.skorohodov01@mail.ru>
2022-08-11 12:33:25 +0300

    update mainfile

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ bot = telebot.TeleBot("5424627277:AAEMba6dgSMkNyoznHAx_LfSK6kclTfWqNQ")
 response = requests.get("https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest?CMC_PRO_API_KEY=f46b82c6-947c-4fb9-89d1-0cd39a4a3675&id=10908")
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+	bot.reply_to(message, "Howdy, how are you doing?")
 
 @bot.message_handler(commands=['go'])
 def send_welcome_go(message):
-    price = response.json()['data']['10908']['quote']['USD']['price']
-    bot.send_message(message, round(price, 4))
+	price = response.json()['data']['10908']['quote']['USD']['price']
+	bot.send_message(message.chat.id, round(price, 5))
 
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
-    bot.send_message(message, "i don't know that you tell me, try pls!")
+	bot.reply_to(message, message.text)
 
 bot.infinity_polling()
